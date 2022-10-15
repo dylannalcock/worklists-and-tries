a84aa2a8722e88a0fa11bf4cd146e8cf65bbe4ce
Dylann Alcock <alcock@uw.edu>
2022-10-15 01:34:08 -0700

    Final edits to p1

--- src/main/java/datastructures/dictionaries/HashTrieMap.java
+++ src/main/java/datastructures/dictionaries/HashTrieMap.java
@@ -38,22 +38,102 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
 
     @Override
     public V insert(K key, V value) {
-        throw new NotYetImplementedException();
+        if(key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+
+        V returnVal;
+
+        if(key.isEmpty()) {
+            returnVal = this.root.value;
+        } else {
+            HashTrieNode currentRoot = (HashTrieNode) this.root;
+
+            for(A a: key) {
+                if(!currentRoot.pointers.containsKey(a)) {
+                    currentRoot.pointers.put(a, new HashTrieNode());
+                }
+                currentRoot = currentRoot.pointers.get(a);
+            }
+            returnVal = currentRoot.value;
+            currentRoot.value = value;
+        }
+        if(returnVal == null) {
+            size++;
+        }
+        return returnVal;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+        if(key == null) {
+            throw new IllegalArgumentException();
+        }
+        HashTrieNode currentRoot = (HashTrieNode) this.root;
+        for(A a: key) {
+
+            currentRoot = currentRoot.pointers.get(a);
+            if (currentRoot == null) {
+                return null;
+            }
+        }
+        return currentRoot.value;
     }
 
     @Override
     public boolean findPrefix(K key) {
-        throw new NotYetImplementedException();
+        if(key == null) {
+            throw new IllegalArgumentException();
+        }
+
+        HashTrieNode currentRoot = (HashTrieNode) this.root;
+        for(A a: key) {
+            currentRoot = currentRoot.pointers.get(a);
+            if(currentRoot == null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void delete(K key) {
-        throw new NotYetImplementedException();
+        if(key == null) {
+            throw new IllegalArgumentException();
+        }
+
+        HashTrieNode currentRoot = (HashTrieNode) this.root;
+        HashTrieNode finalRoot = (HashTrieNode) this.root;
+        A finalKey = null;
+
+        if (key.iterator().hasNext()) {
+            finalKey = key.iterator().next();
+        }
+
+        for (A a : key) {
+            if (currentRoot == null) {
+                return;
+            } else if (currentRoot.value != null || currentRoot.pointers.size() > 1) {
+                finalRoot = currentRoot;
+                finalKey = a;
+            }
+            if (!currentRoot.pointers.isEmpty()) {
+                currentRoot = currentRoot.pointers.get(a);
+            } else {
+                return;
+            }
+        }
+
+        if (currentRoot != null && currentRoot.value != null) {
+            if (!currentRoot.pointers.isEmpty()) {
+                currentRoot.value = null;
+            } else if (finalKey != null) {
+                finalRoot.pointers.remove(finalKey);
+            } else {
+                this.root.value = null;
+            }
+            size--;
+        }
     }
 
     @Override
